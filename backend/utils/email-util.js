2ded7e718fe56c0b714c5d899f65ecd87125d118
UsaaryanByte07 <usaaryan79@gmail.com>
2026-03-17 11:21:43 +0530

    fix(backend): update nodemailer config for render outbound port 465

--- backend/utils/email-util.js
+++ backend/utils/email-util.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
     pass: process.env.GOOGLE_APP_PASSWORD,
